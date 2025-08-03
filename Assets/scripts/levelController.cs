52088e49de52b3495c531c7f63e9e7b0b186775d
ProgramZer0 <nickeljaws@gmail.com>
2025-08-02 20:00:08 -0600

    level start

---
+++ Assets/scripts/levelController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private Vector3[] stagePostions; 
+
+    public int stageNumber = 0;
+
+    private bool unlocked = false;
+
+    void Start()
+    {
+        
+    }
+
+}
